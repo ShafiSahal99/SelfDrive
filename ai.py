14fd5eb58bc9100d157718e92177e1582de5e27f
ShafiSahal99 <shafisahal99@gmail.com>
2021-05-05 14:25:14 +0530

    Completed update function

--- ai.py
+++ ai.py
@@ -17,5 +17,50 @@ class NetWork(nn.Module):
         self.fc2 = nn.Linear(30, nb_action)
         
     def forward(self, state):
+        x = F.relu(self.fc1(state))
+        q_values = self.fc2(x)
+        return q_vallues
+    
+class ReplayMemory(object):
+    
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        
+    def push(self, event):
+        self.memory.append(event)
+        if len(self.memory) > self.capacity:
+            del self.memory[0]
+            
+    def sample(self, batch_size):
+        samples = zip(*random.samle(self.memory, batch_size))
+        return map(lambda x: Variable(torch.cat(x, 0)), samples)
+    
+class Dqn():
+    
+    def __init__(self, input_size, batch_size, gamma):
+        self.gamma = gamma
+        self.reward_window = []
+        self.model = Network(input_size, batch_size)
+        self.memory = ReplayMemory(100000)
+        self.optimizer = optim.Adam(self.model.parameters(), lr = 0.001)
+        self.last_action = 0
+        self.last_reward = 0
+        
+    def select_action(self, state):
+        probs = F.softmax(self.model(Variable(state, volatile = True)) * 100)
+        action = probs.multinomial(num_samples = 1)
+        return action.data[0,0]
+    
+    def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
+        outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
+        next_outputs = self.model(batch_next_state).detach().max(1)[0]
+        target = self.gamma * next_outputs + batch_reward
+        td_loss = F.smooth_l1_loss(outputs, target)
+        self.optimizer.zero_grad()
+        td_loss.backward(retain_graph = True)
+        self.optimizer.step()
+        
+        
         
 
